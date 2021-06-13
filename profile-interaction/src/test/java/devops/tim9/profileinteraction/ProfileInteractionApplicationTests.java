c669fe69dff61432bae94c69f9ba0ef3fa0019a1
MarinaVojnovic <40699105+MarinaVojnovic@users.noreply.github.com>
2021-06-13 12:30:39 +0200

    Added configuration for profile interaction service and dockerfile.

---
+++ profile-interaction/src/test/java/devops/tim9/profileinteraction/ProfileInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.tim9.profileinteraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
