24ecd77e10dabc4e677f60468258269494db9627
MarinaVojnovic <40699105+MarinaVojnovic@users.noreply.github.com>
2021-06-20 12:48:26 +0200

    minor changes

--- profile-interaction/src/main/java/devops/tim9/profileinteraction/ProfileInteractionApplication.java
+++ profile-interaction/src/main/java/devops/tim9/profileinteraction/ProfileInteractionApplication.java
@@ -3,9 +3,6 @@ package devops.tim9.profileinteraction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableFeignClients
 @SpringBootApplication
