071ae224419f8e0f2799144b754b07124ea7882a
LeonaNedeljkovic <leona.nedeljkovic@gmail.com>
2021-07-05 20:31:10 +0200

    19# profile interaction

--- profile-interaction/src/test/java/devops/tim9/profileinteraction/ProfileInteractionApplicationTests.java
+++ profile-interaction/src/test/java/devops/tim9/profileinteraction/ProfileInteractionApplicationTests.java
@@ -2,8 +2,20 @@ package devops.tim9.profileinteraction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 
+@EnableKafka
 @SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(
+  partitions = 1, 
+  controlledShutdown = false,
+  brokerProperties = {
+      "listeners=PLAINTEXT://localhost:9093", 
+      "port=9093"
+})
 class ProfileInteractionApplicationTests {
 
 	@Test
